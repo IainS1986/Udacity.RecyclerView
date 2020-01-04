367e184e314513d3526e683c5652845db03a9a0b
Iain <i_stanford@hotmail.com>
2020-01-04 15:20:52 +0000

    Lesson 6
    ViewHolder binding done

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -43,7 +44,10 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
-        holder.textView.text = data[position].sleepQuality.toString()
+        val item = data[position]
+
+        holder.textView.text = item.sleepQuality.toString()
+        holder.textView.setTextColor(if (item.sleepQuality < 1) Color.RED else Color.BLACK)
     }
 
 }
