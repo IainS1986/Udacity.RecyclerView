6a04b1451e8d62ce2fc72d3fac0a1e2f46c0713a
Iain <i_stanford@hotmail.com>
2020-01-04 16:32:52 +0000

    Lesson 13
    ViewHolders now using Binding objects. Mind blown a bit with how bindings work at compile time to generate companion classes, that you can then use in your code and it "just works" in terms of linking up the Layout to the View code.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -17,7 +17,6 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
@@ -28,6 +27,7 @@ import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
 class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
@@ -39,12 +39,12 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
         holder.bind(getItem(position))
     }
 
-    class ViewHolder private constructor(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder private constructor(val binding : ListItemSleepNightBinding) : RecyclerView.ViewHolder(binding.root) {
         val res = itemView.context.resources
 
-        val sleepLength : TextView = itemView.findViewById(R.id.sleep_length)
-        val quality : TextView = itemView.findViewById(R.id.quality_string)
-        val qualityImage : ImageView = itemView.findViewById(R.id.quality_image)
+        val sleepLength : TextView = binding.sleepLength
+        val quality : TextView = binding.qualityString
+        val qualityImage : ImageView = binding.qualityImage
 
         fun bind(item: SleepNight) {
             sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
@@ -64,8 +64,9 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
 
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_sleep_night, parent, false)
-                return ViewHolder(view)
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
     }
